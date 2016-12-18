0eebaa4659293f871aa723541036e2778973889d
camboui <mathias.branger@gmail.com>
2016-12-18 21:18:58 +0100

    add comments

--- ProjetSkipList/SkipList.h
+++ ProjetSkipList/SkipList.h
@@ -30,47 +30,53 @@ SkipList<V>::SkipList(SkipListNode<V>* first, SkipListNode<V>* last) :
 }
 
 
+//Retourne le noeud qui a la clé "key"
 template<typename V>
 SkipListNode<V>* SkipList<V>::find(int key)
 {
-	SkipListNode<V>* current(m_head);
-	if (m_head != nullptr && m_head->getKey() == key) {
+	SkipListNode<V>* current(m_head); // On part de la tête
+	if (m_head != nullptr && m_head->getKey() == key) { //on regarde si la tête a la valeur recherchée
 		return m_head;
 	}
-	while (current->getKey() != m_tail->getKey()) {
+	while (current->getKey() != m_tail->getKey()) {//Tant que le noeud courant n'a pas la clé recherchée
 		int i = 0;
+		//On parcourt toutes les couches jusqu'à celle qui a la valeur la plus proche de la clé recherchée
 		while (i < current->getAllNext().size() && current->getNext(i)->getKey() > key) i++;
 		if (i == current->getAllNext().size()) return nullptr;
-		current = current->getNext(i);
-		if (current->getKey() == key) return current;
+		current = current->getNext(i);//On actualise le noeud courant
+		if (current->getKey() == key) return current;//Si on a trouvé la clé, on termine
 	}
-	return nullptr;
+	return nullptr;// On ne trouve rien
 }
 
 template<typename V>
 Path<V> SkipList<V>::shortestPath(int fromKey, int toKey)
 {
-	vector<V> path = vector<V>();
-	SkipListNode<V>* current(nullptr);
+	vector<V> path = vector<V>(); // Initialisation du path
+	SkipListNode<V>* current(nullptr); //Pointeur de noeud de skiplist
+										// Si la clé de départ est supérieure à la clé d'arrivée, on renvoit un path vide.
 	if (fromKey > toKey) {
 		cout << "Impossible because key " << fromKey << " is after key " << toKey << endl;
 		return  Path<V>(vector<V>());
 	}
 	else {
-		current = this->find(fromKey);
+		current = this->find(fromKey); // On trouve le noeud correspond à la clé
+									   //S'il existe, on regarde si c'est le noeud terminal, on retourne le path
 		if (current != nullptr) {
 			if (fromKey == toKey) {
 				path.push_back(current->getValue());
 				return Path<V>(path);
 			}
+			//Sinon, on continue de chercher la clé d'arrivée tant qu'on ne la trouve pas ou qu'on arrive à la fin de la liste.
 			else {
 				while (current->getKey() != m_tail->getKey()) {
 					int i = 0;
 					path.push_back(current->getValue());
+					//On parcours tous les noeuds suivants pour savoir où on va
 					while (i < current->getAllNext().size() && current->getNext(i)->getKey() > toKey) i++;
 					if (i == current->getAllNext().size()) return Path<V>(vector<V>());
 					current = current->getNext(i);
-					if (current->getKey() == toKey) {
+					if (current->getKey() == toKey) { // On a trouvé le noeud d'arrivée
 						path.push_back(current->getValue());
 						return Path<V>(path);
 					}
@@ -78,10 +84,9 @@ Path<V> SkipList<V>::shortestPath(int fromKey, int toKey)
 				return Path<V>(vector<V>());
 			}
 		}
-		else {
-			return Path<V>(vector<V>());
-		}
+		else { return Path<V>(vector<V>()); }
 	}
 }
+
 
 #endif
--- ProjetSkipList/TrainLineParser.cpp
+++ ProjetSkipList/TrainLineParser.cpp
@@ -16,13 +16,13 @@ TrainLineParser::TrainLineParser(string filename)
 void makeLevel(vector<int> keys, vector<SkipListNode<Station>*> stations) {
 	SkipListNode<Station>* current(nullptr);
 	SkipListNode<Station>* next(nullptr);
-	for (int i = 0; i < stations.size(); i++) {
+	for (int i = 0; i < stations.size(); i++) { //On cherche le noeud courant
 		if (stations[i]->getKey() == keys[0]) {
 			current = stations[i];
 			break;
 		}
 	}
-	if (current == nullptr) return;
+	if (current == nullptr) return; // On cherche qui doit être le suivant
 	for (int i = 1; i < keys.size(); i++) {
 		for (int j = 0; j < stations.size(); j++) {
 			if (stations[j]->getKey() == keys[i]) {
@@ -30,7 +30,7 @@ void makeLevel(vector<int> keys, vector<SkipListNode<Station>*> stations) {
 				break;
 			}
 		}
-		if (next == nullptr) break;
+		if (next == nullptr) break; // On ajoute le suivant au courant
 		current->addNext(next);
 		current = next;
 	}
@@ -38,32 +38,31 @@ void makeLevel(vector<int> keys, vector<SkipListNode<Station>*> stations) {
 
 Line TrainLineParser::parseFile()
 {
-	if (m_file.is_open())
-	{
-		vector<SkipListNode<Station>*> stations = vector<SkipListNode<Station>*>();
-		vector<int> keys = vector<int>();
-		string station;
+	if (m_file.is_open()) {
+		vector<SkipListNode<Station>*> stations = vector<SkipListNode<Station>*>(); // Initialisation de la skiplist
+		vector<int> keys = vector<int>(); // Initialisation de la liste des clés
+		string station; // nom de la station
 		int nbStations, nbStationsTotal, previous;
 		int key;
-		m_file >> nbStations;
+		m_file >> nbStations; // on récupère le nombre de station
 		nbStationsTotal = nbStations;
 		previous = nbStations;
-		getline(m_file, station); //ignored end of first line
-		for (int i = 1; i <= nbStations; i++) {
+		getline(m_file, station); //on ignore la fin de la première ligne
+		for (int i = 1; i <= nbStations; i++) { //On ajoute chaque station à la skipList la plus basse
 			getline(m_file, station);
-			stations.push_back(new SkipListNode<Station>(i, Station(station,i)));
+			stations.push_back(new SkipListNode<Station>(i, Station(station, i)));
 			keys.push_back(i);
 		}
-		makeLevel(keys, stations);
-		while (!m_file.eof()) {	
+		makeLevel(keys, stations); //on fait la liaison entre les noeuds
+		while (!m_file.eof()) {	//on continue de lire les stations
 			m_file >> nbStations;
-			keys.clear();
+			keys.clear(); // On réinitialise les clés lues
 			if (nbStations <= previous && nbStations > 0) {
-				for (int i = 0; i < nbStations; i++) {
+				for (int i = 0; i < nbStations; i++) { //On lit les stations déservies
 					m_file >> key;
-					keys.push_back(key);
+					keys.push_back(key); // On les ajoute à celles désservies par le train
 				}
-				makeLevel(keys, stations);
+				makeLevel(keys, stations); // On ajoute toutes les stations déservies par le train à la ligne
 				std::cout << endl;
 				previous = nbStations;
 			}
@@ -72,7 +71,7 @@ Line TrainLineParser::parseFile()
 			}
 		}
 		m_file.close();
-		return Line(stations[0], stations[nbStationsTotal - 1]);
+		return Line(stations[0], stations[nbStationsTotal - 1]); //Retourner la ligne construite
 	}
 	throw logic_error("file not open");
 }
